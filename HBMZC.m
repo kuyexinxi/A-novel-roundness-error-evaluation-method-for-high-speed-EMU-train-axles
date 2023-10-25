000c577596471b71be917a5981579dc43469444e
kuyexinxi <148989092+kuyexinxi@users.noreply.github.com>
2023-10-26 01:29:11 +0800

    Add files via upload

---
+++ HBMZC.m
@@ -0,0 +1,9 @@
+clc
+clear all
+ZB = readtable('Dataset4.xlsx');
+x=ZB{:,1};
+y=ZB{:,2};
+%% Obtaining the quasi-MZC center through Bayesian Linear Regression
+[xb,yb,zb,vls]=BYS(x,y);
+%% Obtaining the MZC center and calculating the MZC roundness error
+[Oq,cp,Om,f]=MZC(x,y,xb,yb,zb,vls); 
